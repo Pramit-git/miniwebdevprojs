70c3beab3875b615d2467bf782cf0dad98f2f2c4
Pramit git <pramitsarkarclg@gmail.com>
2025-08-23 09:43:47 +0530

    Added key event program

---
+++ key event project/script.js
@@ -0,0 +1,34 @@
+const myBox = document.getElementById("box")
+const  move = 10;
+let x = 0;
+let y = 0;
+
+document.addEventListener("keydown", event =>{
+    if(event.key.startsWith("Arrow")){
+        event.preventDefault();
+        switch (event.key){
+            case "ArrowUp":
+                y = y-move;
+                break;
+            case "ArrowDown":
+                y = y+move;
+                break;
+            case "ArrowLeft":
+                x = x-move;
+                break;
+            case "ArrowRight":
+                x = x+move; 
+                break;  
+        }
+        myBox.style.top = `${y}px`;
+        myBox.style.left = `${x}px`;
+    }
+});
+document.addEventListener("keydown",event =>{
+    myBox.textContent = "😘";
+    myBox.style.backgroundColor = "yellow";
+})
+document.addEventListener("keyup",event =>{
+    myBox.textContent = "😊";
+    myBox.style.backgroundColor = "aqua";
+})
